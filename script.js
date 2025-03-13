a4c2b8dd891f803d06fd6d9077d11cb053c793a0
katarasaheb <cafebymistake00@gmail.com>
2025-03-13 02:29:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Simple JavaScript for form submission handling or animation
+document.querySelectorAll('form').forEach(form => {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Thank you for signing up!');
+    });
+});
 
